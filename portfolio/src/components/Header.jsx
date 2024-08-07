35a49113fa69e915bf3a0d5cefb18d2fde333869
Hiba-Mohamed <hibamoha987@gmail.com>
2024-08-07 18:51:21 -0230

    tailwind update, header cleanup

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -4,31 +4,12 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { TfiClose } from "react-icons/tfi";
 
 export function Heading() {
-  const [open, setOpen] = useState(false);
   return (
     <nav className="border-slate-50 bg-white font-OpenSans">
       <div className="max-w-2xl flex flex-wrap items-center justify-between mx-auto px-4 max-w-7xl">
         <div id="navbar-hamburger">
           <ul className="hidden text-sm md:flex flex gap-6 flex-row items-center font-medium rounded-lg md:pt-2 md:pb-2">
-            <li>
-              <Link
-                to="/"
-                className="block font-bold py-2 pl-3 pr-4 text-gray-900 rounded hover:text-gray-50 border border-violet-950 rounded-full hover:bg-violet-950 hover:text-gray-50"
-                onClick={() => setOpen(!open)}
-              >
-                Home
-              </Link>
-            </li>
 
-            <li>
-              <Link
-                to="/projects"
-                className="block font-bold py-2 pl-3 pr-4 text-gray-900 rounded hover:text-gray-50 border border-violet-950 rounded-full hover:bg-violet-950 hover:text-gray-50"
-                onClick={() => setOpen(!open)}
-              >
-                Projects
-              </Link>
-            </li>
 
             <li>
               <a
@@ -36,7 +17,6 @@ export function Heading() {
                 target="_blank"
                 rel="noopener noreferrer"
                 className="block font-bold py-2 pl-3 pr-4 text-gray-900 rounded hover:text-gray-50 border border-violet-950 rounded-full hover:bg-violet-950 hover:text-gray-50"
-                onClick={() => setOpen(!open)}
               >
                 {" "}
                 GitHub
@@ -44,49 +24,17 @@ export function Heading() {
             </li>
           </ul>
         </div>
-        <div className="flex flex-row items-center gap-4 hidden md:flex"></div>
-        <button
-          onClick={() => setOpen(!open)}
-          data-collapse-toggle="navbar-hamburger"
-          type="button"
-          className="md:hidden inline-flex items-center justify-center p-2 w-10 h-10 ml-3 text-sm text-gray-500 rounded-lg hover:bg-sky-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-          aria-controls="navbar-hamburger"
-          aria-expanded="false"
-        >
-          {open ? <TfiClose /> : <RxHamburgerMenu />}
-        </button>
         <div
-          className={`${open ? "block" : "hidden"} w-full`}
           id="navbar-hamburger"
         >
           <ul className="md:hidden flex flex-col items-center font-medium my-4 rounded-lg">
-            <li>
-              <Link
-                to="/"
-                className="block font-bold py-2 pl-3 pr-4 text-gray-900 rounded hover:text-sky-600"
-                aria-current="page"
-                onClick={() => setOpen(!open)}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/projects"
-                className="block font-bold py-2 pl-3 pr-4 text-gray-900 rounded hover:text-sky-600"
-                onClick={() => setOpen(!open)}
-              >
-                Projects
-              </Link>
-            </li>
-
+          
             <li>
               <a
                 href="http://github.com/Hiba-Mohamed"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="block font-bold py-2 pl-3 pr-4 text-gray-900 rounded hover:text-sky-600"
-                onClick={() => setOpen(!open)}
               >
                 {" "}
                 GitHub
