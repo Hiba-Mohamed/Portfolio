91b40f9e889d166db3d1cf504c7ac5e43eef19cb
Hiba-Mohamed <hibamoha987@gmail.com>
2024-08-09 18:28:32 -0230

    skills section

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import AboutMe from "../components/AboutMe";
+import Skills from "../components/Skills";
 const Home = () => {
   return (
     <>
       <div className="min-h-screen items-centerfont-OpenSans flex flex-col items-center max-w-xl sm:max-w-full">
         <Banner />
         <AboutMe />
+        <Skills />
       </div>
     </>
   );
