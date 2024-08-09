8001fb53507760287ef3cbcb965444ef3ac0a9ce
Hiba-Mohamed <hibamoha987@gmail.com>
2024-08-09 16:50:29 -0230

    desktop icon responsiveness

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -3,6 +3,25 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      spacing: {
+        100: "25rem", // 400px
+        104: "26rem", // 416px
+        108: "27rem", // 432px
+        112: "28rem", // 448px
+        116: "29rem", // 464px
+        120: "30rem", // 480px
+        128: "32rem", // 512px
+        136: "34rem", // 544px
+        144: "36rem", // 576px
+        152: "38rem", // 608px
+        160: "40rem", // 640px
+        176: "44rem", // 704px
+        192: "48rem", // 768px
+        208: "52rem", // 832px
+        224: "56rem", // 896px
+        240: "60rem", // 960px
+        256: "64rem", // 1024px
+      },
       backgroundImage: {
         banner: "url('omer-yildiz-U7rwm4WmIPA-unsplash.jpg')",
       },
